3a507bc1a8a1d2a1b4f028710ba5bd336ec7b008
Anh Dao Tuan <anhdtuet1995@gmail.com>
2022-09-16 16:50:29 +0700

    Init ads

--- app/src/main/java/com/anhdt/androidlogcat/MainActivity.java
+++ app/src/main/java/com/anhdt/androidlogcat/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.databinding.DataBindingUtil;
 
 import com.androidnetworking.AndroidNetworking;
@@ -17,12 +18,29 @@ import com.anhdt.androidlogcat.databinding.ActivityMainBinding;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import vcc.viv.ads.bin.AdsData;
 import vcc.viv.ads.bin.AdsLogger;
 import vcc.viv.ads.bin.AdsManager;
+import vcc.viv.ads.bin.AdsManagerCallback;
+import vcc.viv.ads.bin.AdsRequest;
 import vcc.viv.ads.bin.InitializeParameter;
+import vcc.viv.ads.bin.Zone;
 import vcc.viv.ads.bin.adsenum.AdsBrowser;
+import vcc.viv.ads.bin.adsenum.AdsForm;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    /**
+     * Nếu trên 1 màn hình mỗi zoneid chỉ có hiển thị ở 1 nơi duy nhất thì tham số này không quan trọng, fix 1 String bất kì nhưng không empty  hoặc null
+     */
+    private String requestId = "1";
+    private String zoneId = "2026943";
+
+    private String userId = "userId_1000223";
 
     private ActivityMainBinding binding;
     private AdsManager adsManager;
@@ -55,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
                         });
             }
         }).start();
-
     }
 
     public void initAdsSdk(Activity activity, String name, String email, String phone, String address, String sessionId) {
@@ -65,6 +82,31 @@ public class MainActivity extends AppCompatActivity {
                 .setWebBrowser(AdsBrowser.inapp, null); // optional
         adsManager = AdsManager.getInstance();
         adsManager.initialize(parameter);
+
+        // Đăng ký nhận callback về màn hình
+        adsManager.callbackRegister(TAG, new AdsManagerCallback() {
+            @Override
+            public void initSuccess() {
+                super.initSuccess();
+                // Gửi request lấy quảng cáo
+                adsManager.request(TAG, requestId, new AdsRequest.ReaderParameter(userId, new ArrayList<Zone>() {{
+                    add(new Zone(zoneId));
+                }}, new ArrayList<String>() {{
+                    add("1");
+                }}, "https://app.kenh14.vn/home&v=5.1.51", "https://app.kenh14.vn/home"));
+            }
+
+            @Override
+            public void requestAdsSuccess(String id, String requestId, List<AdsManager.AdsInfo> adsInfo) {
+                super.requestAdsSuccess(id, requestId, adsInfo);
+                if (!TAG.equals(id)) return;
+                runOnUiThread(() -> {
+                    ConstraintLayout constraintLayout = findViewById(R.id.ads_parent);
+                    // Add quảng cáo vào view
+                    AdsData info = adsManager.addAds(AdsForm.popup, constraintLayout, TAG, requestId, adsInfo.get(0).zoneId);
+                });
+            }
+        });
     }
 
 }
